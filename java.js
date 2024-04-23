7616c8576feb6daa41b88fd8bde5a8c6cfba614c
KINGOFMUSIC <GoldonGame.06@gmail.com>
2024-04-23 13:09:17 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,27 @@
+const addTask = document.getElementById('addTask');
+const addList = document.getElementById('addList');
+const alertMessage = document.getElementById('alert')
+function newList(){
+    alertMessage.innerHTML = '';
+    if(addTask.value === ''){
+        let h5 = document.createElement('h5');
+        h5.innerHTML = "Вы ввели пустую строку";
+        alertMessage.appendChild(h5);
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = addTask.value;
+        addList.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+}
+addList.addEventListener('click', function(event){
+    if(event.target.tagName === 'LI'){
+        event.target.classList.toggle('checked');
+    }
+    else if(event.target.tagName === 'SPAN'){
+        event.target.parentElement.remove();
+    }
+}, false)
